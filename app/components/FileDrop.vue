4ed3bd523b423fef5a269c03427236899e2bfe6f
metkm <metinkorkmaz417@gmail.com>
2025-07-28 00:33:42 +0300

    file drop add button too

--- app/components/FileDrop.vue
+++ app/components/FileDrop.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import type { UnlistenFn } from '@tauri-apps/api/event'
 import { getCurrentWebview } from '@tauri-apps/api/webview'
+import { open } from '@tauri-apps/plugin-dialog'
 
 const path = defineModel<string>()
 const hovering = ref(false)
+
+const openFile = async () => {
+  path.value = await open({
+    multiple: false,
+    directory: false,
+  }) ?? undefined
+}
 
 let unlistenFn: UnlistenFn
 
@@ -25,7 +33,7 @@ onUnmounted(() => unlistenFn())
 
 <template>
   <div
-    class="flex-1 flex items-center justify-center rounded-(--ui-radius) transition-colors"
+    class="flex-1 flex flex-col items-center justify-center gap-2 rounded-(--ui-radius) transition-colors"
     :class="{ 'bg-elevated/50': hovering }"
   >
     <span class="bg-elevated size-10 shrink-0 flex items-center justify-center rounded-full">
@@ -34,5 +42,13 @@ onUnmounted(() => unlistenFn())
         class="text-muted"
       />
     </span>
+
+    <UButton
+      variant="soft"
+      color="neutral"
+      @click="openFile"
+    >
+      Select File
+    </UButton>
   </div>
 </template>
