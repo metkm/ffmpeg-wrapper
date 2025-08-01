d705e0aba3862da5c30987153210c72935b542fb
metkm <metinkorkmaz417@gmail.com>
2025-08-01 12:17:31 +0300

    animations woow

--- app/components/FileDrop.vue
+++ app/components/FileDrop.vue
@@ -2,6 +2,7 @@
 import type { UnlistenFn } from '@tauri-apps/api/event'
 import { getCurrentWebview } from '@tauri-apps/api/webview'
 import { open } from '@tauri-apps/plugin-dialog'
+import { motion } from 'motion-v'
 
 const emit = defineEmits<{
   drop: []
@@ -10,6 +11,7 @@ const emit = defineEmits<{
 const path = defineModel<string>()
 
 const hovering = ref(false)
+const hoveringPath = ref<string>()
 
 const openFile = async () => {
   path.value = await open({
@@ -23,10 +25,13 @@ let unlistenFn: UnlistenFn | undefined
 onMounted(async () => {
   unlistenFn = await getCurrentWebview()
     .onDragDropEvent((event) => {
-      if (event.payload.type === 'over') {
+      if (event.payload.type === 'enter') {
+        hoveringPath.value = event.payload.paths.at(0)
+      } else if (event.payload.type === 'over') {
         hovering.value = true
       } else if (event.payload.type === 'leave') {
         hovering.value = false
+        hoveringPath.value = undefined
       } else if (event.payload.type === 'drop') {
         path.value = event.payload.paths.at(0)
         emit('drop')
@@ -40,15 +45,33 @@ onUnmounted(() => unlistenFn?.())
 <template>
   <div class="flex flex-col flex-1 gap-2 rounded-(--ui-radius) overflow-hidden">
     <button
-      class="flex-1 flex items-center justify-center h-full w-full hover:bg-muted/50 transition-colors"
+      class="flex flex-col items-center justify-center h-full w-full hover:bg-muted/50 transition-colors"
       @click="openFile"
     >
-      <span class="p-4 shrink-0 flex items-center justify-center rounded-full border border-dashed border-muted">
-        <UIcon
-          name="i-lucide-upload"
-          class="text-muted size-8"
-        />
-      </span>
+      <LayoutGroup>
+        <AnimatePresence>
+          <motion.span
+            layout
+            class="p-4 shrink-0 flex items-center justify-center rounded-full border border-dashed border-muted"
+          >
+            <UIcon
+              name="i-lucide-upload"
+              class="text-muted size-8"
+            />
+          </motion.span>
+
+          <motion.p
+            v-if="hoveringPath"
+            layout
+            class="font-medium text-muted mt-4"
+            :initial="{ opacity: 0 }"
+            :animate="{ opacity: 1 }"
+            :exit="{ opacity: 0 }"
+          >
+            {{ hoveringPath.split('\\').at(-1) }}
+          </motion.p>
+        </AnimatePresence>
+      </LayoutGroup>
     </button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', 'motion-v/nuxt'],
   ssr: false,
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
