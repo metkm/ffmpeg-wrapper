f30b019c91cfb0ca451be46a4ef605db7872784c
metkm <metinkorkmaz417@gmail.com>
2025-10-25 18:47:43 +0300

    remove avif from export

--- app/constants.ts
+++ app/constants.ts
@@ -10,4 +10,4 @@ export const imageExtensions = ['.jpeg', '.png', '.jpg'] as const satisfies stri
 export const resolutions = ['640x480', '1280x720', '1920x1080', '2560x1440'] as const satisfies string[]
 
 export const videoImportExtensions = ['mp4', 'avi', 'mov'] as const satisfies string[]
-export const videoExportExtensions = [...videoImportExtensions, 'webp', 'png', 'avif'] as const satisfies string[]
+export const videoExportExtensions = [...videoImportExtensions, 'webp', 'png'] as const satisfies string[]
