42e438a98108fa96172f5d7eaad2b93306a4983a
metkm <metinkorkmaz417@gmail.com>
2025-10-25 15:29:30 +0300

    remove unused constants

--- app/constants.ts
+++ app/constants.ts
@@ -1,55 +1,12 @@
 import type { DialogFilter } from '@tauri-apps/plugin-dialog'
 import type { Video } from './types/video'
 
-export const defaultVideoValues: Video = {
-  duration: 0,
-  currentTime: 0,
-  durationRange: [0, 1],
-  volume: 0,
-}
-
 export const encoders = [
   'av1_nvenc',
   'h264_nvenc',
   'libx264',
   'libx265',
 ]
-
-// export const defaultEncoderOptions = {
-//   name: 'cq:v',
-//   min: 0,
-//   max: 51,
-//   default: 1,
-//   type: 'inputnumber',
-//   description: '0 = auto',
-// }
-
-// export const parametersPerEncoders = {
-//   av1_nvenc: [
-//     {
-//       id: 1,
-//       ...defaultEncoderOptions,
-//     },
-//   ],
-//   h264_nvenc: [
-//     {
-//       id: 3,
-//       ...defaultEncoderOptions,
-//     },
-//   ],
-//   libx264: [
-//     {
-//       id: 2,
-//       ...defaultEncoderOptions,
-//     },
-//   ],
-//   libx265: [
-//     {
-//       id: 4,
-//       ...defaultEncoderOptions,
-//     },
-//   ],
-// } as const
 
 export const videoFilters: DialogFilter[] = [{
   name: 'video',
