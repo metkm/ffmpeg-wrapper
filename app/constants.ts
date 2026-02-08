4243b978ad71b6da8e96417e6572fce40b2fdea9
metkm <metinkorkmaz417@gmail.com>
2026-02-08 21:45:30 +0300

    delete old commands in constants

--- app/constants.ts
+++ app/constants.ts
@@ -72,12 +72,3 @@ export const outputExtensionSelectItems: SelectItem[] = [
   { type: 'separator' },
   ...animatedExtensionNames,
 ]
-
-// const videoExtensions = ['mp4', 'avi', 'mov', 'dvr'] as const satisfies string[]
-// const imageExtensions = ['png', 'jpg'] as const satisfies string[]
-// const imageAnimatedExtensions = ['webp', 'avif'] as const satisfies string[]
-
-// export const videoImportExtensions = videoExtensions
-// export const videoExportExtensions = [...videoExtensions, ...imageExtensions, ...imageAnimatedExtensions]
-
-// export const videoExportItems = [...videoExtensions, { type: 'separator' }, ...imageExtensions, { type: 'separator' }, ...imageAnimatedExtensions] satisfies SelectItem[]
