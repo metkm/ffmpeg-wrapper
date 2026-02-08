311fe07a31e94b75c01481c0de7ac909dc12d670
metkm <metinkorkmaz417@gmail.com>
2026-02-08 12:36:06 +0300

    css fixes for overflows on the folder page

--- app/App.vue
+++ app/App.vue
@@ -31,7 +31,7 @@ onMounted(async () => {
         </template>
       </Suspense>
 
-      <main class="h-full w-full min-h-0 min-w-0 relative *:p-2">
+      <main class="flex h-full w-full min-h-0 min-w-0 relative *:p-2 overflow-y-auto">
         <NuxtPage />
       </main>
     </div>
